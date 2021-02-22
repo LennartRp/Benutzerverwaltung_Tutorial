37ece63471cd6339269b198cc6ab3799093c9723
di39258 <tobias.bodo@dillinger.biz>
2021-02-22 13:03:08 +0100

    hinzufuegen applicationService Klasse BenutzerService

---
+++ src/main/java/de/dillinger/stw/azubi/lennart/applicationservice/BenutzerService.java
@@ -0,0 +1,34 @@
+package de.dillinger.stw.azubi.lennart.applicationservice;
+
+public class BenutzerService
+{
+
+    private final String name;
+    private final String vorName;
+    private final String email;
+
+    public BenutzerService(String name, String vorName, String email)
+    {
+        this.vorName = vorName;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String legeBenutzerAn()
+    {
+
+    }
+    public String loescheBenutzer()
+    {
+
+    }
+    public String benutzerAendern()
+    {
+
+    }
+    public String sucheBenutzer()
+    {
+        
+    }
+
+}
